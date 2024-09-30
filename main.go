9301ae74bd2722f42baeb68988bc4efe7dda450c
Dunn, Simeon <simedunn01@gmail.com>
2024-09-30 17:05:48 -0400

    create payment intent route

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"os"
@@ -10,6 +11,7 @@ import (
 	"github.com/pocketbase/pocketbase"
 	"github.com/pocketbase/pocketbase/core"
 	"github.com/stripe/stripe-go/v79"
+	"github.com/stripe/stripe-go/v79/paymentintent"
 )
 
 type CheckoutData struct {
@@ -36,9 +38,63 @@ func main() {
 		return nil
 	})
 
+	// route: public key
+	app.OnBeforeServe().Add(func(e *core.ServeEvent) error {
+		e.Router.GET("/public-key", publicKeyHandler)
+		return nil
+	})
+
+	// route: create payment intent
+	app.OnBeforeServe().Add(func(e *core.ServeEvent) error {
+		e.Router.POST("/create-payment-intent", handleCreatePaymentIntent)
+		return nil
+	})
+
 	// start pocketbase server
 	app_err := app.Start()
 	if app_err != nil {
 		log.Fatal(app_err)
 	}
+}
+
+type PublicKeyParams struct {
+	StripeKey string `json:"key"`
+}
+
+func publicKeyHandler(c echo.Context) (err error) {
+	data := PublicKeyParams{
+		StripeKey: os.Getenv("STRIPE_PUBLISHABLE_KEY"),
+	}
+	return c.JSON(http.StatusOK, data)
+}
+
+func handleCreatePaymentIntent(c echo.Context) (err error) {
+	params := &stripe.PaymentIntentParams{
+		Amount:   stripe.Int64(7000),
+		Currency: stripe.String(string(stripe.CurrencyUSD)),
+		AutomaticPaymentMethods: &stripe.PaymentIntentAutomaticPaymentMethodsParams{
+			Enabled: stripe.Bool(true),
+		},
+	}
+
+	pi, err := paymentintent.New(params)
+	if err != nil {
+		if stripeErr, ok := err.(*stripe.Error); ok {
+			fmt.Printf("Stripe error: %v\n", stripeErr.Msg)
+			return c.JSON(http.StatusInternalServerError, echo.Map{
+				"error": stripeErr.Msg,
+			})
+		} else {
+			fmt.Printf("Other error occurred: %v\n", err.Error())
+			return c.JSON(http.StatusInternalServerError, echo.Map{
+				"error": err.Error(),
+			})
+		}
+	}
+
+	return c.JSON(http.StatusOK, struct {
+		ClientSecret string `json:"clientSecret"`
+	}{
+		ClientSecret: pi.ClientSecret,
+	})
 }
