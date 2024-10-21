679abb8ef4ee6cb190d6fdb9a9fb92a387db6f48
Dunn, Simeon <simedunn01@gmail.com>
2024-10-21 15:05:21 -0400

    alias for billingSession

--- main.go
+++ main.go
@@ -13,6 +13,7 @@ import (
 	"github.com/pocketbase/pocketbase"
 	"github.com/pocketbase/pocketbase/core"
 	"github.com/stripe/stripe-go/v79"
+	billingSession "github.com/stripe/stripe-go/v79/billingportal/session"
 	"github.com/stripe/stripe-go/v79/checkout/session"
 	"github.com/stripe/stripe-go/v79/customer"
 	"github.com/stripe/stripe-go/v79/webhook"
@@ -247,4 +248,28 @@ func handleSubscriptionDeleted(sub *stripe.Subscription, app *pocketbase.PocketB
 	}
 
 	return nil
+}
+
+func handleCustomerPortal(c echo.Context) error {
+	var requestBody struct {
+		CustomerId string `json:"customerId"`
+	}
+
+	if err := c.Bind(&requestBody); err != nil {
+		return c.String(http.StatusBadRequest, "Invalid customer ID")
+	}
+
+	stripe.Key = os.Getenv("STRIPE_SECRET_KEY")
+
+	customerPortalSession := &stripe.BillingPortalSessionParams{
+		Customer:  stripe.String(requestBody.CustomerId),
+		ReturnURL: stripe.String(os.Getenv("STRIPE_CUSTOMER_PORTAL_RETURN_URL")),
+	}
+
+	result, err := billingSession.New(customerPortalSession)
+	if err != nil {
+		return c.String(http.StatusInternalServerError, err.Error())
+	}
+
+	return c.Redirect(http.StatusOK, result.URL)
 }
